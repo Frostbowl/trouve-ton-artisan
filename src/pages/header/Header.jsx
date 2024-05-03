4191ab3369a8939dabdc95cf9662e358b4caee76
Frostbowl <masclet.c@gmail.com>
2024-05-03 22:33:02 +0400

    add responsive design and end header + nav

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -4,31 +4,22 @@ import "./Header.css";
 
 const Header = () => {
     return(
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <p>Coucou test font</p>
+    <nav class="navbar navbar-expand-lg ">
         <div class="container-fluid">
           <Link class="navbar-brand" to="#"><img className="logo" src="../img/logo.png" alt="Trouve ton artisant"/></Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Bâtiment</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Fabrication</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Alimentation</a>
-              </li>
-            </ul>
+          <div class="collapse navbar-collapse headBar" id="navbarSupportedContent">
+            <div className="navigation">
+              <Link class="nav-link" aria-current="page" to="#">Bâtiment</Link>                 
+              <Link class="nav-link " aria-current="page" to="#">Services</Link>      
+              <Link class="nav-link " aria-current="page" to="#">Fabrication</Link>
+              <Link class="nav-link " aria-current="page" to="#">Alimentation</Link>
+            </div>
             <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button class="btn btn-outline-success" type="submit">Search</button>
+              <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search"/>
+              <button class=" " type="submit">Search</button>
             </form>
           </div>
         </div>
