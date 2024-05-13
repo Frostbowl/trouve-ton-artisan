cbc94fbcec90bd757d731f1e508067d9e0c4aaf4
Frostbowl <masclet.c@gmail.com>
2024-05-13 16:07:50 +0400

    ajout CSS page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../assets/style/not-found.css';
 
 const NotFound = () => {
 
     return(
-        <div>
-            <h1>La page recherchée est actuellement introuvable</h1>
-            <Link to="/"><h2>Retour à l'accueil</h2></Link>
+        <div className="notFound">
+            <img src="../img/stop-img.png" alt="stop" className="stopImg"/>
+            <h1>La page que vous recherchez est actuellement introuvable</h1>
+            <h2>Veuillez revenir à <span><Link to='/'>L'accueil</Link></span> </h2>
         </div>
         
     )
